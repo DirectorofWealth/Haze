1a4eca1ce4fea33f1c09bc35569fc774e2ec92f4
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-31 22:49:38 +0100

    feat/fetched the weather api data

--- src/main.js
+++ src/main.js
@@ -4,11 +4,8 @@ const searchInput = document.querySelector("#searchInput");
 
 const searchButton = document.querySelector("#searchButton");
 
-console.log(searchForm);
-console.log(searchInput);
-console.log(searchButton);
 
-searchForm.addEventListener("submit", (event) => {
+searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const city = searchInput.value.trim();
@@ -18,5 +15,50 @@ searchForm.addEventListener("submit", (event) => {
       return;
     }
 
-    console.log(city);
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+    );
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const {
+      latitude,
+      longitude
+    } = data.results[0]
+
+    console.log(latitude)
+    console.log(longitude)
+
+
+    const weatherResponse = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,cloud_cover,wind_speed_10m`
+    );
+
+    const weatherData = await weatherResponse.json();
+
+    console.log(weatherData);
+
+    const {
+      temperature_2m,
+      relative_humidity_2m,
+      cloud_cover,
+      wind_speed_10m
+  } = weatherData.current;
+
+    console.log(temperature_2m);
+    console.log(relative_humidity_2m);
+    console.log(cloud_cover);
+    console.log(wind_speed_10m);
+
+
+    document.querySelector('#temperature').textContent = `${temperature_2m}°C`;
+    document.querySelector('#humidity').textContent = `${relative_humidity_2m}%`;
+    document.querySelector('#cloud-cover').textContent = `${cloud_cover}%`;
+    document.querySelector('#wind-speed').textContent = `${wind_speed_10m} km/h`;
+
+    document.getElementById("#weather-info").hidden = false;
+
+
 });
