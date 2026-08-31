35d3c2b48222e5ba8736cd069a9fb06a1c0d3536
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-31 23:17:37 +0100

    feat: add user-facing error handling for users wrong input and API failures

--- src/main.js
+++ src/main.js
@@ -5,18 +5,40 @@ const searchInput = document.querySelector("#searchInput");
 const searchButton = document.querySelector("#searchButton");
 
 
+function getWeatherDescription(weather_code) {
+
+    if (weather_code === 0) {
+        return "Clear sky";
+    }
+
+    if (weather_code === 1) {
+        return "Mainly clear";
+    }
+
+    if (weather_code === 2) {
+        return "Partly cloudy";
+    }
+
+    if (weather_code === 3) {
+        return "Overcast";
+    }
+
+    return "Unknown";
+}
+
+
 searchForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const city = searchInput.value.trim();
 
-    if(!city) {
-      console.log("Please enter a city")
-      return;
+    if (!city) {
+        console.log("Please enter a city");
+        return;
     }
 
     const response = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
     );
 
     const data = await response.json();
@@ -24,16 +46,16 @@ searchForm.addEventListener("submit", async (event) => {
     console.log(data);
 
     const {
-      latitude,
-      longitude
-    } = data.results[0]
+        latitude,
+        longitude
+    } = data.results[0];
 
-    console.log(latitude)
-    console.log(longitude)
+    console.log(latitude);
+    console.log(longitude);
 
 
     const weatherResponse = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,cloud_cover,wind_speed_10m`
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,cloud_cover,wind_speed_10m,weather_code`
     );
 
     const weatherData = await weatherResponse.json();
@@ -41,11 +63,12 @@ searchForm.addEventListener("submit", async (event) => {
     console.log(weatherData);
 
     const {
-      temperature_2m,
-      relative_humidity_2m,
-      cloud_cover,
-      wind_speed_10m
-  } = weatherData.current;
+        temperature_2m,
+        relative_humidity_2m,
+        cloud_cover,
+        wind_speed_10m,
+        weather_code
+    } = weatherData.current;
 
     console.log(temperature_2m);
     console.log(relative_humidity_2m);
@@ -53,12 +76,25 @@ searchForm.addEventListener("submit", async (event) => {
     console.log(wind_speed_10m);
 
 
-    document.querySelector('#temperature').textContent = `${temperature_2m}°C`;
-    document.querySelector('#humidity').textContent = `${relative_humidity_2m}%`;
-    document.querySelector('#cloud-cover').textContent = `${cloud_cover}%`;
-    document.querySelector('#wind-speed').textContent = `${wind_speed_10m} km/h`;
+    document.querySelector("#temperature").textContent =
+        `${temperature_2m}°C`;
 
-    document.getElementById("#weather-info").hidden = false;
+    document.querySelector("#humidity").textContent =
+        `${relative_humidity_2m}%`;
+
+    document.querySelector("#cloud-cover").textContent =
+        `${cloud_cover}%`;
+
+    document.querySelector("#wind-speed").textContent =
+        `${wind_speed_10m} km/h`;
 
 
+    document.querySelector("#weather-info").hidden = false;
+
+
+    const description = getWeatherDescription(weather_code);
+
+    console.log(description);
+
+    document.querySelector("#condition").textContent = description;
 });
