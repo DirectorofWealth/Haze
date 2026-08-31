722e20a391decee272af1fcb26a1c1500afd33d0
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-31 17:27:32 +0100

    feat/cleaned user inputs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 const searchForm = document.querySelector("#searchForm");
+
 const searchInput = document.querySelector("#searchInput");
+
 const searchButton = document.querySelector("#searchButton");
 
 console.log(searchForm);
 console.log(searchInput);
 console.log(searchButton);
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const city = searchInput.value.trim();
+
+    if(!city) {
+      console.log("Please enter a city")
+      return;
+    }
+
+    console.log(city);
+});
